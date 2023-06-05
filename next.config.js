71c47ec425a118d18c1a14e9e451a017f64cf24e
Goat95 <hjh9703@naver.com>
2023-06-06 03:15:35 +0900

    feat: implements songs item components

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["uofkcslsioyhubkbnpqo.supabase.co"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
